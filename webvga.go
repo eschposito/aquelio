d88565d45d1c9a67b72908a4b82a8dbb0793b29b
Antonio Esposito <aespo@tiscali.it>
2018-02-28 14:01:06 +0100

    Minor changes

--- webvga.go
+++ webvga.go
@@ -2,42 +2,53 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"io/ioutil"
+	"net/http"
+	"sync/atomic"
+	//"github.com/luismesas/goPi/spi"
+	"golang.org/x/exp/io/spi"
 	//"time"
 )
 
-var vmem [4][4000]byte
+var vmem [4][4000]byte // 4 sectors: 1 is written to while 1 is displayed
+var readindex uint32 = 0 // atomic access first index of displayed vmem sector
 
 func vramHandler(w http.ResponseWriter, req *http.Request) {
-    //w.Header().Set("Content-Type", "text/plain")
-    w.Write(vmem[0][0:4000])
-    // fmt.Fprintf(w, "This is an example server.\n")
-    // io.WriteString(w, "This is an example server.\n")
+	ind := atomic.LoadUint32(&readindex)
+	w.Write(vmem[ind&3][0:4000]) // sends data of sector to be displayed
 }
 
 func clickHandler(w http.ResponseWriter, req *http.Request) {
-    //w.Header().Set("Content-Type", "text/plain")
-    //w.Write(vmem[0][0:4000])
-    // fmt.Fprintf(w, "This is an example server.\n")
-    // io.WriteString(w, "This is an example server.\n")
+	w.WriteHeader(200) // empty OK HTTP response
 }
 
 func main() {
 	axspw, err := ioutil.ReadFile("axspw.txt")
 	if err != nil {
-        fmt.Println("File read error: ", err)
-    }
+		fmt.Println("File read error: ", err)
+	}
 	exepw, err := ioutil.ReadFile("exepw.txt")
 	if err != nil {
-        fmt.Println("File read error: ", err)
-    }
-    http.HandleFunc("/vram~" + string(axspw) + "/", vramHandler)
-	http.HandleFunc("/vram~" + string(exepw) + "/", vramHandler)
-	http.HandleFunc("/click~" + string(exepw) + "/", clickHandler)
+		fmt.Println("File read error: ", err)
+	}
+	http.HandleFunc("/vram~"+string(axspw)+"/", vramHandler)
+	http.HandleFunc("/vram~"+string(exepw)+"/", vramHandler)
+	http.HandleFunc("/click~"+string(exepw)+"/", clickHandler)
 	http.Handle("/", http.FileServer(http.Dir("pub")))
-    err = http.ListenAndServeTLS(":25681", "cert.pem", "key.pem", nil)
-    if err != nil {
-        fmt.Println("ListenAndServeTLS error: ", err)
-    }
+	err = http.ListenAndServeTLS(":25681", "cert.pem", "key.pem", nil)
+	if err != nil {
+		fmt.Println("ListenAndServeTLS error: ", err)
+	}
+}
+
+func useSpi() {
+dev, err := spi.Open(&spi.Devfs{
+    Dev:      "/dev/spidev0.1",
+    Mode:     spi.Mode3,
+    MaxSpeed: 500000,
+})
+if err != nil {
+    panic(err)
+}
+defer dev.Close()
 }
